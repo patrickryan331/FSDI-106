82b983f25db088626616f29d2abf86c0892b44b0
patrickryan331 <patrickryan331@gmail.com>
2024-06-13 22:44:40 -0400

    Class 3

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,4 @@
+// class 1 functions
 // function sayHello(){
 //     console.log("Hello there");
 // }
@@ -12,11 +13,56 @@
 //     sayGoodbye();
 // }
 
-function saveTask(){
+
+function saveTask()
+{
     console.log("Saving tasks");
+    // get the values
+        const title = $("#txtTitle").val();
+        const description = $("#txtDescription").val();
+        const color = $("#selColor").val();
+        const date = $("#selDate").val();
+        const status = $("#selStatus").val();
+        const budget = $("#numBudget").val();
+        console.log(title,description,color,date,status,budget);
+    // build an object
+    let taskToSave = new Task(title,description,color,date,status,budget);
+    console.log(taskToSave);
+
+    // save to the server
+
+
+    // display the info from the server
+    displayTask(taskToSave);
+
+    
 }
 
-function init(){
+
+function displayTask(task)
+{
+    let syntax = `
+    <div class='task'>
+        <div class='info'>
+            <h3>${task.title}</h3>
+            <h5>${task.description}</h5>
+        </div>
+            <label class="status">${task.status}</label>
+            <div class="date-budget">
+                <label>${task.date}</label>
+                <label>${task.budget}</label>
+            </div>
+    </div>
+        `
+    $(".list-task").append(syntax);
+}
+
+
+
+
+
+function init()
+{
     console.log("task manager");
 
     //load data
---
+++ scripts/task.js
@@ -0,0 +1,12 @@
+class Task{
+    constructor(title,desc,color,date,status,budget)
+    {
+        this.title = title;
+        this.description = desc;
+        this.color = color;
+        this.date = date;
+        this.status = status;
+        this.budget = budget;
+        this.name = "patrick";
+    }   
+}
